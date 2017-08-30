64144e714bd188b21bd72ace5f3bc8072dad810c
Jesus Moreno <jesusm@exsilio.com>
2017-08-29 18:35:03 -0700

    added formatting

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone } from "@angular/core"; 
+import { Component, NgZone } from "@angular/core";
 import { LocationService } from "../services/locationService.service";
 import { FacebookGraphApi } from "../services/facebookApi/facebookGraphApi.service";
 
@@ -11,12 +11,12 @@ let appSettings = require("tns-core-modules/application-settings");
     providers: [LocationService, FacebookGraphApi]
 })
 export class HomeComponent {
-public latitude: number;
-public longitude: number;
-public currentLocation: string;
-public avatarUrl: string;
-public userName: string;
-public userId: string;
+    public latitude: number;
+    public longitude: number;
+    public currentLocation: string;
+    public avatarUrl: string;
+    public userName: string;
+    public userId: string;
 
     constructor(private zone: NgZone
         , private location: LocationService
@@ -26,18 +26,17 @@ public userId: string;
         this.currentLocation = "";
     }
 
-    public enableLocationTap() { 
-        if(!this.location.serviceLocation())
-            {
-                this.location.getDeviceLocation();
-            }
-            else {
-                this.currentLocation = "GPS Location Service On!";
-            }
+    public enableLocationTap() {
+        if (!this.location.serviceLocation()) {
+            this.location.getDeviceLocation();
+        }
+        else {
+            this.currentLocation = "GPS Location Service On!";
+        }
     }
 
-   public buttonGetLocationTap() {
-    this.location.getDeviceLocation().then(result => {
+    public buttonGetLocationTap() {
+        this.location.getDeviceLocation().then(result => {
             this.latitude = result.latitude;
             this.longitude = result.longitude;
         }, error => {
@@ -50,21 +49,21 @@ public userId: string;
 
     public getFBInfo() {
         this.FB.getFacebookInfo2()
-        .subscribe(result => {
-            this.userName = this.FB.user.name;
-            this.userId = this.FB.user.id;
-        })
-    }
-    public getFBAvatar(){
-        if(this.FB.user){
-            this.FB.getFacebookAvatar()
             .subscribe(result => {
-                console.log(result.data.url);
-                this.avatarUrl = result.data.url;
+                this.userName = this.FB.user.name;
+                this.userId = this.FB.user.id;
             })
+    }
+    public getFBAvatar() {
+        if (this.FB.user) {
+            this.FB.getFacebookAvatar()
+                .subscribe(result => {
+                    console.log(result.data.url);
+                    this.avatarUrl = this.FB.user.avatar//result.data.url;
+                })
         }
     }
-    public logout(){
+    public logout() {
         this.FB.logOut();
     }
 }
--- app/services/facebookApi/facebookGraphApi.service.ts
+++ app/services/facebookApi/facebookGraphApi.service.ts
@@ -2,66 +2,64 @@ import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import { Observable } from "rxjs/Rx";
 import "rxjs/add/operator/map";
-// import { Observable } from 'tns-core-modules/data/observable';
 import { FacebookUser } from "./facebookUser";
 import * as tnsOAuthModule from 'nativescript-oauth';
 
 let appSettings = require("tns-core-modules/application-settings");
-// let http = require("tns-core-modules/http");
 let config = require("../../app.config").config;
 
 @Injectable()
-export class FacebookGraphApi { 
+export class FacebookGraphApi {
     userId: string;
     accessToken: string;
     user: FacebookUser;
-    
-constructor(private http: Http){
-}
 
-public logIntoFacebook() {
-    tnsOAuthModule.ensureValidToken()
-    .then((token: string)=>{
-        appSettings.setString("access_token", token);
-        this.accessToken = token;
-        console.log('token: ' + token);
-    })
-    .catch((er)=>{
-        console.log(er);
-    });    
-}
+    constructor(private http: Http) {
+    }
 
-public logOut() {
-    tnsOAuthModule.logout()
-    .then(()=>{
-        console.log("Logged out of Facebook!!!");
-    })
-}
+    public logIntoFacebook() {
+        tnsOAuthModule.ensureValidToken()
+            .then((token: string) => {
+                appSettings.setString("access_token", token);
+                this.accessToken = token;
+                console.log('token: ' + token);
+            })
+            .catch((er) => {
+                console.log(er);
+            });
+    }
 
-public getFacebookInfo2(){
-    return this.http.get(config.FACEBOOK_GRAPH_API_URL + "/me?access_token=" + this.accessToken)
-    .map((res) => res.json())
-     .map(data => {
-         this.user = new FacebookUser(data.id, data.name, "");
-         console.log("User Id and Name set...fetching avatar url...");
-         return this.user;
-    })
-    .catch(this.handleErrors);
-}
+    public logOut() {
+        tnsOAuthModule.logout()
+            .then(() => {
+                console.log("Logged out of Facebook!!!");
+            })
+    }
 
-public getFacebookAvatar() {
-    return this.http.get(config.FACEBOOK_GRAPH_API_URL + "/" + this.user.id + "/picture?type=large&redirect=false&access_token=" + this.accessToken )
-    .map((res)=> res.json())
-    .map(data => {
-        this.user.avatar = data.data.url;
-        return data;
-    })
-    .catch(this.handleErrors)
-    
-}
+    public getFacebookInfo2() {
+        return this.http.get(config.FACEBOOK_GRAPH_API_URL + "/me?access_token=" + this.accessToken)
+            .map((res) => res.json())
+            .map(data => {
+                this.user = new FacebookUser(data.id, data.name, "");
+                console.log("User Id and Name set...fetching avatar url...");
+                return this.user;
+            })
+            .catch(this.handleErrors);
+    }
 
-handleErrors(error: Response) {
-    console.log(JSON.stringify(error.json()));
-    return Observable.throw(error);
-  }
+    public getFacebookAvatar() {
+        return this.http.get(config.FACEBOOK_GRAPH_API_URL + "/" + this.user.id + "/picture?type=large&redirect=false&access_token=" + this.accessToken)
+            .map((res) => res.json())
+            .map(data => {
+                this.user.avatar = data.data.url;
+                return data;
+            })
+            .catch(this.handleErrors)
+
+    }
+
+    handleErrors(error: Response) {
+        console.log(JSON.stringify(error.json()));
+        return Observable.throw(error);
+    }
 }
